987d65e329d1a5c706f7611a5b79c17bd7920ee8
Catherine <kankatya@bk.ru>
2022-05-02 15:55:35 +0500

    Add tasks 2, 2.1, 3

---
+++ Task_2.1/Program.cs
@@ -0,0 +1,31 @@
+﻿//Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+Console.Write("Введите порядковое число дня недели -> ");
+string DayOfWeek = Console.ReadLine();
+switch (DayOfWeek)
+{
+    case "1":
+        Console.WriteLine("Сегодня понедельник. Сожалею!");
+        break;
+    case "2":
+        Console.WriteLine("Сегодня вторник. Желаю удачи!");
+        break;
+    case "3":
+        Console.WriteLine("Сегодня среда. Бывало и лучше!");
+        break;
+    case "4":
+        Console.WriteLine("Сегодня четверг. Держитесь! Осталось совсем немного!");
+        break;
+    case "5":
+        Console.WriteLine("Сегодня пятница. Ю-хуууу!");
+        break;
+    case "6":
+        Console.WriteLine("Сегодня суббота. Поздравляю! Вы пережили эту неделю!");
+        break;
+    case "7":
+        Console.WriteLine("Сегодня воскресенье. Готовьтесь к следующей неделе!");
+        break;
+     default:
+        Console.WriteLine("Не понятно т.т");
+        break;
+}
---
+++ Task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+
+
+string DayOfWeek = Console.ReadLine();
+if(DayOfWeek == "1") Console.WriteLine("Понедельник");
+else if(DayOfWeek == "2") Console.WriteLine("Вторник");
+else if(DayOfWeek == "3") Console.WriteLine("Среда");
+else if(DayOfWeek == "4") Console.WriteLine("Четверг");
+else if(DayOfWeek == "5") Console.WriteLine("Пятница");
+else if(DayOfWeek == "6") Console.WriteLine("Суббота");
+else if(DayOfWeek == "7") Console.WriteLine("Воскресенье");
+else Console.WriteLine("Ошибка ввода"); 
